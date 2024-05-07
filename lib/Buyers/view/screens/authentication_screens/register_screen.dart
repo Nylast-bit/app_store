9347aa0bb894a3b99ddd223536a2d9de3e40de4c
Nylast-bit <stalyn.fernandez27@gmail.com>
2024-05-07 11:07:37 -0400

    Adding some register components

--- lib/Buyers/view/screens/authentication_screens/register_screen.dart
+++ lib/Buyers/view/screens/authentication_screens/register_screen.dart
@@ -6,7 +6,29 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Register'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Register your new account',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color(0xffffffff),
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 30,),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                prefixIcon: Icon(Icons.email),
+                labelText: "Enter your email",
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
